895de3ef9ccd00b25e9453fa3ba2f6eb5a93a58b
gal kahana <gal.kahana@hotmail.com>
2015-05-16 20:47:36 +0300

    icu - Temporary switch to my own version supporting node12

--- hummusrenderer.js
+++ hummusrenderer.js
@@ -2,7 +2,7 @@ var hummus = require('hummus'),
 	tmp = require('temporary'),
 	http = require('http'),
 	https = require('https'),
-	bidi = require('icu-bidi'),
+	bidi = require('gal-icu-bidi'),
 	fs = require('fs');
 
 
